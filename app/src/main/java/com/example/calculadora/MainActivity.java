5245389ebec657f203d694d6007a9f64f59a68db
ArturoBorja <53122722+ArturoBorja@users.noreply.github.com>
2019-08-29 13:06:25 -0500

    2 calculadoras
    Ambas calculadoras funcionan

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -6,14 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_pro;
+    Button ban_samar;
+    Button ban_restart;
+    Button ban_multiplicand;
+    Button ban_divider;
+    EditText ext_01;
+    EditText ext_02;
+    TextView tat_raspiest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_pro=findViewById(R.id.btn_pro);
+        ban_samar =findViewById(R.id.btn_sumar);
+        ban_restart =findViewById(R.id.btn_restar);
+        ban_divider =findViewById(R.id.btn_dividir);
+        ban_multiplicand =findViewById(R.id.btn_multiplicar);
+        ext_01=findViewById(R.id.editText);
+        ext_02=findViewById(R.id.editText2);
+        tat_raspiest =findViewById(R.id.textView);
+
         btn_pro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -21,5 +39,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(calculadorapro,1);
             }
         });
+        ban_samar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tat_raspiest.setText(String.valueOf(Double.valueOf(ext_01.getText().toString())+Double.valueOf(ext_02.getText().toString())));
+            }
+        });
+        ban_restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tat_raspiest.setText(String.valueOf(Double.valueOf(ext_01.getText().toString())-Double.valueOf(ext_02.getText().toString())));
+            }
+        });
+        ban_multiplicand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tat_raspiest.setText(String.valueOf(Double.valueOf(ext_01.getText().toString())*Double.valueOf(ext_02.getText().toString())));
+            }
+        });
+        ban_divider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tat_raspiest.setText(String.valueOf(Double.valueOf(ext_01.getText().toString())/Double.valueOf(ext_02.getText().toString())));
+            }
+        });
     }
+
 }
