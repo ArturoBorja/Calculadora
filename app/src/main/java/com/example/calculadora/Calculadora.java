84df14b858dcc5ab90a760c76d45bb66e7a46d19
ArturoBorja <53122722+ArturoBorja@users.noreply.github.com>
2019-08-29 12:13:25 -0500

    terminada Calculadora pro
    la calculadora pro ejecuta todos los comandos y guarda una memoria de ejecución con varios símbolos y una secuencia de ejecución así como un registro de todos los números que se han desarrollado operado o ingresado

--- app/src/main/java/com/example/calculadora/Calculadora.java
+++ app/src/main/java/com/example/calculadora/Calculadora.java
@@ -17,14 +17,16 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.regex.Pattern;
 
 public class Calculadora extends AppCompatActivity {
     Button[] btn_numero=new Button[10];
-    Button btn_dot, btn_x100, btn_borrar, btn_rest, btn_suma, btn_div, btn_por, btn_enter;
+    Button btn_dot,btn_x100,btn_borrar,btn_rest,btn_suma,btn_div,btn_por,btn_enter,btn_exp,btn_squa;
     TextView txt_opera;
     TextView txt_00;
     List<Double> memoria;
     List<String> operadores;
+    int a001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +35,9 @@ public class Calculadora extends AppCompatActivity {
         txt_opera=findViewById(R.id.txt_opera);
         txt_00=findViewById(R.id.txt_00);
         txt_00.setMovementMethod(new ScrollingMovementMethod());
-        memoria=new ArrayList<>();
-        operadores=new ArrayList<>();
+        memoria=new ArrayList<Double>();
+        operadores=new ArrayList<String>();
+
         CrearNumero();
         EscribirNumero();
     }
@@ -57,6 +60,8 @@ public class Calculadora extends AppCompatActivity {
         btn_div=findViewById(R.id.btn_div);
         btn_por=findViewById(R.id.btn_por);
         btn_enter=findViewById(R.id.btn_enter);
+        btn_squa=findViewById(R.id.btn_squa);
+        btn_exp=findViewById(R.id.btn_exp);
 
     }
     void  EscribirNumero(){
@@ -78,46 +83,92 @@ public class Calculadora extends AppCompatActivity {
         btn_suma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txt_00.append(txt_opera.getText().toString()+"\n");
-                memoria.add(Double.valueOf(txt_opera.getText().toString()));
-                operadores.add("+");
-                txt_opera.setText("");
+                if(txt_opera.getText().toString().equals("")){
+
+                }else {
+                    txt_00.append(txt_opera.getText().toString() + "\n");
+                    memoria.add(Double.valueOf(txt_opera.getText().toString()));
+                    operadores.add("+");
+                    txt_opera.setText("");
+                }
             }
         });
         btn_rest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txt_00.append(txt_opera.getText().toString()+"\n");
-                memoria.add(Double.valueOf(txt_opera.getText().toString()));
-                operadores.add("-");
-                txt_opera.setText("");
+                if(txt_opera.getText().toString().equals("")){
+
+                }else {
+                    txt_00.append(txt_opera.getText().toString() + "\n");
+                    memoria.add(Double.valueOf(txt_opera.getText().toString()));
+                    operadores.add("-");
+                    txt_opera.setText("");
+                }
             }
         });
         btn_div.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txt_00.append(txt_opera.getText().toString()+"\n");
-                memoria.add(Double.valueOf(txt_opera.getText().toString()));
-                operadores.add("/");
-                txt_opera.setText("");
+                if(txt_opera.getText().toString().equals("")){
+
+                }else {
+                    txt_00.append(txt_opera.getText().toString() + "\n");
+                    memoria.add(Double.valueOf(txt_opera.getText().toString()));
+                    operadores.add("/");
+                    txt_opera.setText("");
+                }
             }
         });
         btn_por.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txt_00.append(txt_opera.getText().toString()+"\n");
-                memoria.add(Double.valueOf(txt_opera.getText().toString()));
-                operadores.add("*");
-                txt_opera.setText("");
+                if(txt_opera.getText().toString().equals("")){
+
+                }else {
+                    txt_00.append(txt_opera.getText().toString() + "\n");
+                    memoria.add(Double.valueOf(txt_opera.getText().toString()));
+                    operadores.add("*");
+                    txt_opera.setText("");
+                }
             }
         });
         btn_x100.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txt_00.append(txt_opera.getText().toString()+"\n");
-                memoria.add(Double.valueOf(txt_opera.getText().toString()));
-                operadores.add("%");
-                txt_opera.setText("");
+                if(txt_opera.getText().toString().equals("")){
+
+                }else {
+                    txt_00.append(txt_opera.getText().toString() + "\n");
+                    memoria.add(Double.valueOf(txt_opera.getText().toString()));
+                    operadores.add("%");
+                    txt_opera.setText("");
+                }
+            }
+        });
+        btn_squa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(txt_opera.getText().toString().equals("")){
+
+                }else {
+                    txt_00.append(txt_opera.getText().toString() + "\n");
+                    memoria.add(Double.valueOf(txt_opera.getText().toString()));
+                    operadores.add("r");
+                    txt_opera.setText("");
+                }
+            }
+        });
+        btn_exp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(txt_opera.getText().toString().equals("")){
+
+                }else {
+                    txt_00.append(txt_opera.getText().toString() + "\n");
+                    memoria.add(Double.valueOf(txt_opera.getText().toString()));
+                    operadores.add("p");
+                    txt_opera.setText("");
+                }
             }
         });
         btn_enter.setOnClickListener(new View.OnClickListener() {
@@ -125,58 +176,97 @@ public class Calculadora extends AppCompatActivity {
             public void onClick(View view) {
                 double total;
                 String cope;
-                cope = operadores.get(operadores.size()-1);
-                txt_00.append(txt_opera.getText().toString()+"\n");
-                memoria.add(Double.valueOf(txt_opera.getText().toString()));
-                txt_opera.setText("");
-                switch(cope){
-                    case "+":
-                        total=(memoria.get((memoria.size()-1)))+(memoria.get((memoria.size()-2)));
-                        memoria.remove(memoria.size()-1);
-                        memoria.remove(memoria.size()-1);
-                        operadores.remove(operadores.size()-1);
-                        txt_opera.append(String.valueOf(total));
-                        break;
-                    case "-":
-                        total=(memoria.get((memoria.size()-2)))-(memoria.get((memoria.size()-1)));
-                        memoria.remove(memoria.size()-1);
-                        memoria.remove(memoria.size()-1);
-                        operadores.remove(operadores.size()-1);
-                        txt_opera.append(String.valueOf(total));
-                        break;
-                    case "/":
-                        total=(memoria.get((memoria.size()-2)))/(memoria.get((memoria.size()-1)));
-                        memoria.remove(memoria.size()-1);
-                        memoria.remove(memoria.size()-1);
-                        operadores.remove(operadores.size()-1);
-                        txt_opera.append(String.valueOf(total));
-                        break;
-                    case "*":
-                        total=(memoria.get((memoria.size()-1)))*(memoria.get((memoria.size()-2)));
-                        memoria.remove(memoria.size()-1);
-                        memoria.remove(memoria.size()-1);
-                        operadores.remove(operadores.size()-1);
-                        txt_opera.append(String.valueOf(total));
-                        break;
-                    case "%":
-                        total=(memoria.get((memoria.size()-2)))*((memoria.get((memoria.size()-1)))/100);
-                        memoria.remove(memoria.size()-1);
-                        memoria.remove(memoria.size()-1);
-                        operadores.remove(operadores.size()-1);
-                        txt_opera.append(String.valueOf(total));
-                        break;
-                    default:
+                if(txt_opera.getText().toString().equals("")){
 
-                        break;
+                }else if(operadores.size()!=0) {
+                    cope = operadores.get(operadores.size() - 1);
+                    txt_00.append(txt_opera.getText().toString() + "\n");
+                    memoria.add(Double.valueOf(txt_opera.getText().toString()));
+                    txt_opera.setText("");
+                    switch (cope) {
+                        case "+":
+                            total = (memoria.get((memoria.size() - 1))) + (memoria.get((memoria.size() - 2)));
+                            memoria.remove(memoria.size()-1);
+                            memoria.remove(memoria.size()-1);
+                            operadores.remove(operadores.size() - 1);
+                            txt_opera.append(String.valueOf(total));
+                            break;
+                        case "-":
+                            total = (memoria.get((memoria.size() - 2))) - (memoria.get((memoria.size() - 1)));
+                            memoria.remove(memoria.size()-1);
+                            memoria.remove(memoria.size()-1);
+                            operadores.remove(operadores.size() - 1);
+                            txt_opera.append(String.valueOf(total));
+                            break;
+                        case "/":
+                            total = (memoria.get((memoria.size() - 2))) / (memoria.get((memoria.size() - 1)));
+                            memoria.remove(memoria.size()-1);
+                            memoria.remove(memoria.size()-1);
+                            operadores.remove(operadores.size() - 1);
+                            txt_opera.append(String.valueOf(total));
+                            break;
+                        case "*":
+                            total = (memoria.get((memoria.size() - 1))) * (memoria.get((memoria.size() - 2)));
+                            memoria.remove(memoria.size()-1);
+                            memoria.remove(memoria.size()-1);
+                            operadores.remove(operadores.size() - 1);
+                            txt_opera.append(String.valueOf(total));
+                            break;
+                        case "%":
+                            total = (memoria.get((memoria.size() - 2))) * ((memoria.get((memoria.size() - 1))) / 100);
+                            memoria.remove(memoria.size()-1);
+                            memoria.remove(memoria.size()-1);
+                            operadores.remove(operadores.size() - 1);
+                            txt_opera.append(String.valueOf(total));
+                            break;
+                        case "r":
+                            total = Math.pow(memoria.get((memoria.size() - 2)) , (1/memoria.get((memoria.size() - 1))));
+                            memoria.remove(memoria.size()-1);
+                            memoria.remove(memoria.size()-1);
+                            operadores.remove(operadores.size() - 1);
+                            txt_opera.append(String.valueOf(total));
+                            break;
+                        case "p":
+                            total = Math.pow(memoria.get(memoria.size() - 2) , memoria.get(memoria.size() - 1)) ;
+                            memoria.remove(memoria.size()-1);
+                            memoria.remove(memoria.size()-1);
+                            operadores.remove(operadores.size() - 1);
+                            txt_opera.append(String.valueOf(total));
+                            break;
+                        default:
+
+                            break;
+                    }
                 }
-
-
             }
         });
         btn_borrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txt_opera.setText("");
+                String[] memo = txt_00.getText().toString().split(Pattern.quote("\n"));
+                if (txt_opera.getText().toString().equals("")) {
+                    if(txt_00.getText().toString().equals("")){
+
+                    }else {
+                        txt_00.setText("");
+                        for (int a = 0; a < memo.length - 1; a++) {
+                            if (memo[a].equals("")) {
+
+                            } else{
+                                txt_00.append(memo[a] + "\n");
+                            }
+                        }
+                        txt_opera.setText(memo[memo.length-1]);
+                    }
+                    if(operadores.size()!=0){
+                        operadores.remove(operadores.size()-1);
+                    }
+                    if(memoria.size()!=0){
+                        memoria.remove(memoria.size()-1);
+                    }
+                } else {
+                    txt_opera.setText("");
+                }
             }
         });
     }
